4b245f60f3a800eedc59431473a011667824a4fc
Vasiliy Samoilichenko <vasiliy.samoilichenko@ewave.com>
2021-09-29 20:02:16 +0300

    DevBox 3.0

--- tools/system/wsl.ps1
+++ tools/system/wsl.ps1
@@ -1,8 +1,10 @@
+. require_once "$devbox_root/tools/system/constants.ps1"
+
 ############################ Public functions ############################
 
 function get_wsl_path($path) {
     $wsl_path = ((Resolve-Path $path) -Replace '\\', '/')
-    $wsl_path = (wsl wslpath $wsl_path)
+    $wsl_path = (wsl -d ${devbox_wsl_distro_name} wslpath $wsl_path)
 
     return $wsl_path
 }
