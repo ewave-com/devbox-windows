7dbcc2d3952b8b9a1d66d4f25b5a037777b405f4
Vasiliy Samoilichenko <vasiliy.samoilichenko@ewave.com>
2021-10-20 17:55:18 +0300

    RC 3.0.2

--- tools/system/ssl.ps1
+++ tools/system/ssl.ps1
@@ -3,14 +3,48 @@
 
 ############################ Public functions ############################
 
-function ssl_import_new_system_certificate($_cert_source_path) {
-    add_system_ssl_certificate "${_cert_source_path}"
+function ssl_add_system_certificate($_cert_source_path = '', $_subject_search_pattern = '', $is_root_ca = $false) {
+    if (-not $_cert_source_path) {
+        show_error_message "Unable to add CA certificate. Filename cannot be empty."
+        exit 1
+    }
+
+    if (-not (Test-Path $_cert_source_path -PathType Leaf)) {
+        show_error_message "Unable to add CA certificate. Cert file does not exist at path '${_cert_source_path}'."
+        exit 1
+    }
+
+    if ($is_root_ca) {
+        $_store_name = 'ROOT'
+    } else {
+        $_store_name = 'DevBox'
+    }
+
+    $_thumbprints = (Get-ChildItem -path "cert:CurrentUser\${_store_name}" -Recurse | where { $_.Subject -like "${_subject_search_pattern}" } | Select Thumbprint)
+    if (!$_thumbprints) {
+        certutil -addstore -user -f "${_store_name}" $_cert_source_path | Out-Null
+    }
 }
 
-function ssl_disable_system_certificate($_cert_source_path) {
-    ssl_remove_system_certificate "${_cert_source_path}"
-}
+function ssl_delete_system_certificate($_cert_source_path = '', $_subject_search_pattern = '', $is_root_ca = $false) {
+    if (-not $_subject_search_pattern) {
+        show_error_message "Unable to remove CA certificate. Search parrent cannot be empty."
+        exit 1
+    }
 
+    if ($is_root_ca) {
+        $_store_name = 'ROOT'
+    } else {
+        $_store_name = 'DevBox'
+    }
+
+    if ($_subject_search_pattern) {
+        $_thumbprints = (Get-ChildItem -path "cert:CurrentUser\${_store_name}" -Recurse | where { $_.Subject -like "${_subject_search_pattern}" } | Select Thumbprint)
+        if ($_thumbprints) {
+            $_thumbprints | ForEach-Object { certutil -delstore -user -Silent "${_store_name}" $_.Thumbprint | Out-Null }
+        }
+    }
+}
 
 function ssl_generate_root_certificate_authority($_target_root_crt_path, $_target_root_key_path) {
     if (-not $_target_root_crt_path) {
@@ -54,7 +88,6 @@ function ssl_generate_root_certificate_authority($_target_root_crt_path, $_targe
         exit 1
     }
 }
-
 
 function ssl_generate_domain_certificate($_website_name, $_extra_domains, $_target_crt_path, $_target_key_path, $_root_ca_pem_path, $_root_ca_key_path) {
     if (-not $_website_name) {
@@ -138,79 +171,6 @@ function ssl_generate_domain_certificate($_website_name, $_extra_domains, $_targ
         exit 1
     }
 }
-#
-# function ssl_generate_domain_certificate($_website_name = "", $_target_crt_path = "", $_target_key_path = "") {
-#     # todo check doc about self-signed certificates
-#     #https://medium.com/better-programming/how-to-create-trusted-ssl-certificates-for-your-local-development-13fd5aad29c6
-#     #https://medium.com/better-programming/trusted-self-signed-certificate-and-local-domains-for-testing-7c6e6e3f9548
-#     if (-not $_website_name) {
-#         show_error_message "Unable to generate CA certificate. Website name cannot be empty."
-#         exit 1
-#     }
-#
-#     if (-not $_target_crt_path) {
-#         show_error_message "Unable to generate CA certificate. Target path of certificate cannot be empty."
-#         exit 1
-#     }
-#
-#     $_cert_basename = [System.IO.Path]::GetFileNameWithoutExtension($_target_crt_path)
-#     if (-not $_target_key_path) {
-#         $_target_key_path = "$( Split-Path -Path $_target_crt_path )/$_cert_basename.key"
-#     }
-#
-#     New-Item -ItemType directory -Path (Split-Path -Path $_target_crt_path) -Force | Out-Null
-#
-#     # use reverse-proxy container to avoid ssl software installation and as this infrastructure container is avaiable for generating
-#     if (is_docker_container_running 'nginx-reverse-proxy') {
-#         $_openssl_command = "[ ! -f /root/.rnd ] && openssl rand -writerand /root/.rnd; " +
-#                 "openssl req -x509 -nodes " +
-#                 "-newkey ec:<(openssl ecparam -name secp384r1) " +
-#                 "-keyout /tmp/${_cert_basename}.key " +
-#                 "-out /tmp/${_cert_basename}.crt " +
-#                 "-days 365 " +
-#                 "-subj '/C=BY/ST=Minsk/L=Minsk/O=DevOpsTeam_EWave/CN=${_website_name}' " +
-#                 " >/dev/null # 2>&1"
-#
-#         docker exec -it 'nginx-reverse-proxy' /bin/bash -c "${_openssl_command}"
-#
-#         docker cp "nginx-reverse-proxy:/tmp/$_cert_basename.crt" "$_target_crt_path"
-#         docker cp "nginx-reverse-proxy:/tmp/$_cert_basename.key" "$_target_key_path"
-#         docker exec -it 'nginx-reverse-proxy' /bin/bash -c "rm /tmp/$_cert_basename.crt /tmp/$_cert_basename.key"
-#     } else {
-#         show_error_message "Unable to generate CA certificate. Nginx-reverse-proxy container for generating is not running."
-#         Exit 1
-#     }
-# }
 
 ############################ Public functions end ############################
 
-############################ Local functions ############################
-
-function add_system_ssl_certificate($_cert_source_path = "") {
-    if (-not $_cert_source_path) {
-        show_error_message "Unable to add CA certificate. Filename cannot be empty."
-        exit 1
-    }
-
-    if (-not (Test-Path $_cert_source_path -PathType Leaf)) {
-        show_error_message "Unable to add CA certificate. Cert file does not exist at path '${_cert_source_path}'."
-        exit 1
-    }
-
-    Start-Process PowerShell -Wait -Verb RunAs -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass", "certutil -addstore -f 'ROOT' $_cert_source_path"
-}
-
-function ssl_remove_system_certificate($_file_name = "", $_website_name = "") {
-    if ((-not $_file_name) -and (-not $_website_name)) {
-        show_error_message "Unable to remove CA certificate. Filename cannot be empty."
-        exit 1
-    }
-
-    if ($_website_name) {
-        $_thumbprint = (Get-ChildItem -path cert: -Recurse | where { $_.Subject -like "CN=$_website_name" } | Select Thumbprint)
-        Start-Process PowerShell -Wait -Verb RunAs -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass", "certutil -delstore -f 'ROOT' $_thumbprint"
-        exit 1
-    }
-}
-
-############################ Local functions end ############################
